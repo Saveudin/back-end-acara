819589bf94cd1c3ccd9139e33a71de6f1458bbb6
Dodi <ronthunder40@gmail.com>
2025-01-29 11:12:29 +0700

    dfgdg

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,13 @@
 import express from "express";
 import router from "./routes/api";
+import bodyParser from "body-parser";
 
 const app = express();
-const PORT = 3000;
+const PORT = 4000;
 
 // middleware
 app.use('/api', router);
+app.use(bodyParser.json());
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
