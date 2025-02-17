e6a0dae36ea958998064a12def9c40841305868b
Dodi <ronthunder40@gmail.com>
2025-02-17 18:35:42 +0700

    Dokumentasi swagger

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,8 @@ import express from "express";
 import router from "./routes/api";
 import bodyParser from "body-parser";
 import db from "./utils/database";
+import docs from "./docs/route";
+import cors from "cors";
 
 const app = express();
 const PORT = 4000;
@@ -15,8 +17,10 @@ async function init (){
         
     
         // middleware
+        app.use(cors());
         app.use(bodyParser.json());
         app.use('/api', router);
+        docs(app);
         app.get('/', (req, res) => {
             res.status(200).json({
                 message: "Server is running",
